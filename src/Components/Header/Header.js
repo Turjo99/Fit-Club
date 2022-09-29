b44c19deec39f180afde5913c584f7ba9b5b263a
SurabBTurjo <84791022+SurabBTurjo@users.noreply.github.com>
2022-09-29 09:09:41 +0600

    Header Done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../Header/Header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <h3>The Fit Club</h3>
+            <p>Exersice The Way you Like Us and</p>
+        </div>
+    );
+};
+
+export default Header;
